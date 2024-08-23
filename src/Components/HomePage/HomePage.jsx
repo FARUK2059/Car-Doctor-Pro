099ebbd00d5ec34024a30dd7731393122f9b25f5
FARUK2059 <omarfaruk34552@gmail.com>
2024-08-24 03:01:17 +0600

    Navbar and MomePage Setup complet

---
+++ src/Components/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+
+
+
+const HomePage = () => {
+    return (
+        <div>
+            <p>TRhis ia Home Page</p>
+        </div>
+    );
+};
+
+export default HomePage;
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,10 @@
+import HomePage from "@/Components/HomePage/HomePage";
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Welcome Car Doctor Pro
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-warning">Warning</button>
-      <button className="btn btn-error">Error</button>
+    <main className="bg-base-100">
+      <HomePage></HomePage>
     </main>
   );
 }
