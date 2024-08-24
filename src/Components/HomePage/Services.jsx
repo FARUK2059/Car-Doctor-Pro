03d77c45c021cd450ea95ffe06d2fd8a4a134af2
FARUK2059 <omarfaruk34552@gmail.com>
2024-08-24 09:31:54 +0600

    Service page Complet

--- src/Components/HomePage/Services.jsx
+++ src/Components/HomePage/Services.jsx
@@ -1,10 +1,44 @@
+import ServiceCard from "./ServiceCard/ServiceCard";
+
+import { services } from '../../ServiceData/ServiceData'
 
 
 
 const Services = () => {
+
+    // const { services } = await getServices();
+
+    // if (services?.length <= 0) {
+    //     return null;
+    // }
+
+    // Menual Inpute Data
+    console.log(services);
+    
+
+
+
+
+
     return (
         <div>
-            <p>This is Service</p>
+            <div className="text-slate-800 mb-24">
+                <div className="text-center container mx-auto">
+                    <h3 className="text-2xl font-bold text-orange-600">Our Services</h3>
+                    <h2 className="text-5xl">Our Service Area</h2>
+                    <p>
+                        the majority have suffered alteration in some form, by injected
+                        humour, or randomised <br /> words which do not look even slightly
+                        believable.{" "}
+                    </p>
+                </div>
+                <div className="container mx-auto mt-12 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-6">
+                    {services?.length > 0 &&
+                        services?.map((service) => (
+                            <ServiceCard service={service} key={service._id} />
+                        ))}
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Sheard/Footer.jsx
+++ src/Components/Sheard/Footer.jsx
@@ -6,14 +6,14 @@ import Link from "next/link";
 const Footer = () => {
     return (
         <div>
-            <div className=" py-12 p-2">
+            <div className=" py-12 p-2 ">
                 <footer className="footer text-base-content container mx-auto">
                     <aside>
                         <Link href={"/"}>
                             <Image alt="logo" src="https://raw.githubusercontent.com/FARUK2059/Car-Doctor-Pro/099ebbd00d5ec34024a30dd7731393122f9b25f5/public/logo.svg" height={60} width={100} />
                         </Link>
                     </aside>
-                    <nav>
+                    <nav className="">
                         <h6 className="footer-title">Useful Links</h6>
                         <a className="link link-hover">About</a>
                         <a className="link link-hover">Services</a>
