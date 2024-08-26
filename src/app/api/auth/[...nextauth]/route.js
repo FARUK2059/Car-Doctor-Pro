3ccfdfcb618ea17e4d572330f790bff05139d106
FARUK2059 <omarfaruk34552@gmail.com>
2024-08-27 02:02:35 +0600

    up sdf

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -47,6 +47,7 @@ const handler = NextAuth({
         //     clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
         //     clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET,
         // }),
+        //  amar sunar bangla ,    ami tumay balobasi
     ],
     pages: {
         signIn: "/login",
