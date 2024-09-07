19bb0dff0593038f8c44d3f86437107fcdabe81e
FARUK2059 <omarfaruk34552@gmail.com>
2024-09-08 01:46:44 +0600

    up 545645df

--- src/Services/middleware.js
+++ src/Services/middleware.js
@@ -5,9 +5,9 @@ import { NextResponse } from "next/server";
 export const middleware = async (request) => {
   const token = cookies(request).get("__Secure-next-auth.session-token");
   const pathname = request.nextUrl.pathname
-  if(pathname.includes('api')) {
-      return NextResponse.next();
-  }
+  // if(pathname.includes('api')) {
+  //     return NextResponse.next();
+  // }
 
   if (!token) {
     return NextResponse.redirect(new URL(`/login?redirect=${pathname}`, request.url));
