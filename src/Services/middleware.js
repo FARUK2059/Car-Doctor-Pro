a8f76103286ac457c375b12531ab07712d817ef1
FARUK2059 <omarfaruk34552@gmail.com>
2024-09-09 04:23:32 +0600

    up jhsdvf24

--- src/Services/middleware.js
+++ src/Services/middleware.js
@@ -1,6 +1,6 @@
 
 // import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
 export const middleware = async (request) => {
   const token = cookies(request).get("__Secure-next-auth.session-token");
